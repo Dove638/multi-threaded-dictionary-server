30cda9f074135241081b3e90ed1a83a93183b6b6
David Xiang <dxxian@student.unimelb.edu.au>
2025-03-24 20:52:03 +1100

    implemented the simpleclient and simpleserver shown in the lecture slides

--- client/src/client/DictionaryClient.java
+++ client/src/client/DictionaryClient.java
@@ -1,4 +1,23 @@
 package client;
+import java.net.*;
+import java.io.*;
 
 public class DictionaryClient {
+    public static void main(String args[]) throws IOException{
+        // Register service on specified port
+        Socket s1 = new Socket("localhost", 1234);
+
+        // Get an input file handle from the ocket and read the input
+        InputStream s1ln = s1.getInputStream();
+        DataInputStream dis  = new DataInputStream(s1ln);
+        String st = new String(dis.readUTF());
+
+        // Print to console the input
+        System.out.println(st);
+
+        // When done, just close the connection and exit
+        dis.close();
+        s1ln.close();
+        s1.close();
+    }
 }
--- server/src/server/DictionaryServer.java
+++ server/src/server/DictionaryServer.java
@@ -1,4 +1,25 @@
 package server;
+import java.net.*;
+import java.io.*;
 
 public class DictionaryServer {
+    public static void main(String args[]) throws IOException{
+        // Register service on port 1234
+        ServerSocket s = new ServerSocket(1234);
+        while (true){
+            Socket s1 = s.accept(); // wait and accept a connection
+
+            // Get a communcation stream associated with the socket
+            OutputStream s1out = s1.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(s1out);
+
+            // Send over info
+            dos.writeUTF("Hi Dave and Anton");
+
+            // Close the connection but not the server socket
+            dos.close();
+            s1out.close();
+            s1.close();
+        }
+    }
 }
